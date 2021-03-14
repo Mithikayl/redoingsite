223594db2efd56e2f04ba5340631311a2bd99919
Mith <66926165+Mithikayl@users.noreply.github.com>
2021-03-14 11:59:41 +0000

    added captions for about page

---
+++ js/captions.js
@@ -0,0 +1,4 @@
+var socialmediacaptions = ['Wow! I have access to the internet!', 'I have social media. Cool.', 'The internet.', 'Ran out of ideas here.']
+const randomSocial = socialmediacaptions[Math.floor(Math.random() * socialmediacaptions.length)];
+
+document.getElementById("socialcaption").innerHTML = randomSocial
