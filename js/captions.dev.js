cb2bb4b0e2d8910075c1a61441020b268609ef45
Mith <66926165+Mithikayl@users.noreply.github.com>
2021-03-15 22:52:42 +0000

    fixed js error - not displaying caption correctly

--- js/captions.dev.js
+++ js/captions.dev.js
@@ -2,7 +2,16 @@
 
 var socialmediacaptions = ['Wow! I have access to the internet!', 'I have social media. Cool.', 'The internet.', 'Ran out of ideas here.'];
 var randomSocial = socialmediacaptions[Math.floor(Math.random() * socialmediacaptions.length)];
-document.getElementById("socialcaption").innerHTML = randomSocial;
+var socialPage = document.getElementById("socialcaption");
+
+if (typeof socialPage != 'undefined' && socialPage != null) {
+  document.getElementById("socialcaption").innerHTML = randomSocial;
+} else {}
+
 var aboutcaptions = ["Yeah... it's me.", 'Yours truly.', 'True gamer here.', 'Yeah.'];
 var randomAbout = aboutcaptions[Math.floor(Math.random() * aboutcaptions.length)];
-document.getElementById("about").innerHTML = randomAbout;
+var aboutPage = document.getElementById("about");
+
+if (typeof aboutPage != 'undefined' && aboutPage != null) {
+  document.getElementById("about").innerHTML = randomAbout;
+} else {}
--- js/captions.js
+++ js/captions.js
@@ -1,10 +1,14 @@
 var socialmediacaptions = ['Wow! I have access to the internet!', 'I have social media. Cool.', 'The internet.', 'Ran out of ideas here.'];
-const randomSocial = socialmediacaptions[Math.floor(Math.random() * socialmediacaptions.length)];
+var randomSocial = socialmediacaptions[Math.floor(Math.random() * socialmediacaptions.length)];
+var socialPage = document.getElementById("socialcaption");
 
-document.getElementById("socialcaption").innerHTML = randomSocial;
-
+if(typeof(socialPage) != 'undefined' && socialPage != null){
+    document.getElementById("socialcaption").innerHTML = randomSocial;
+} else{}
 
 var aboutcaptions = ["Yeah... it's me.", 'Yours truly.', 'True gamer here.', 'Yeah.'];
-const randomAbout = aboutcaptions[Math.floor(Math.random() * aboutcaptions.length)];
-
-document.getElementById("about").innerHTML = randomAbout;
+var randomAbout = aboutcaptions[Math.floor(Math.random() * aboutcaptions.length)];
+var aboutPage = document.getElementById("about");
+if(typeof(aboutPage) != 'undefined' && aboutPage != null){
+    document.getElementById("about").innerHTML = randomAbout;
+} else{}
